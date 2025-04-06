563f697a6b5a6ceb89a45b0673b23a3a7b80297f
takuya <55338+takuya@users.noreply.github.com>
2025-04-06 15:49:06 +0900

    call callback first to blocking in select()

--- src/ProcessExec/Traits/ProcessExecutorWaitHandler.php
+++ src/ProcessExec/Traits/ProcessExecutorWaitHandler.php
@@ -9,8 +9,14 @@ trait ProcessExecutorWaitHandler {
   
   // TODO この機能はObserver側で持つべきかもしれない。
   protected function watcher ( callable $callback ): callable {
-    $last_called_at = microtime( true );
+    $last_called_at = null;
     return function() use ( &$last_called_at, $callback ) {
+      if ( is_null( $last_called_at ) ) {// first time
+        call_user_func( $callback );
+        $last_called_at = microtime( true );
+        return;
+      }
+      //main
       $duration = microtime( true ) - $last_called_at;
       if ( $duration > $this->watch_interval ) {
         call_user_func( $callback );
